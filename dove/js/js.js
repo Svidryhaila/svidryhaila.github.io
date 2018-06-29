0c5b2c4115673c49f9a6b0c0da2d36733766e1b0
Svidryhaila <hatefieldd@gmail.com>
2018-06-29 18:26:50 +0300

    Preloader
    preloader added

--- dove/js/js.js
+++ dove/js/js.js
@@ -315,3 +315,11 @@
 	/*=========ONRESIZE AND MAP/============*/
 
  });
+
+ window.onload = function() {
+ 	var preloader = document.querySelector('.preloader');
+    preloader.style.opacity = '0';
+    setTimeout(function(){
+    	preloader.style.display = 'none';
+    }, 500);
+ };
